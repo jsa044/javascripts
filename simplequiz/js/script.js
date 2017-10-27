019555d2d5c5e7dd451dabc3510bada4f6576268
Jeff Allaire <jeff.allaire@gmail.com>
2017-10-27 13:07:08 -0500

    Worked on logic for quiz in javascript

--- simplequiz/js/script.js
+++ simplequiz/js/script.js
@@ -1,28 +1,30 @@
-
-
-
-function submitAnswers() {
-	
+function submitAnswers() {	
 	var total = 5;
-	var score = 0;
-	
-	//Get user input
-	
+	var score = 0;	
+	//Get user input	
 	var q1 = document.forms["quizForm"] ["q1"].value;
 	var q2 = document.forms["quizForm"] ["q2"].value;
 	var q3 = document.forms["quizForm"] ["q3"].value;
 	var q4 = document.forms["quizForm"] ["q4"].value;
-	var q5 = document.forms["quizForm"] ["q5"].value;
-	
+	var q5 = document.forms["quizForm"] ["q5"].value;	
 	// Validation	
-	
-	for(i=1; i<= total;i++) {
-		
+	for(i=1; i<= total;i++) {		
 		if(eval( 'q'+i) == null || eval('q'+i) =='') {
 		 alert('You missed question ' + i);
 		return false;
-	    }	//End of if statement
-	    	    
-	}	   //End of for loop
-	
+	    }	//End of if statement	    	    
+	}	   //End of Validation 'for' loop	
+	      //Set Correct answers	      
+	var answers = ["b", "a", "d", "b", "d"];	
+	     //Check answers 	     
+	     for(i=1; i<= total;i++) {
+	     	if(eval( 'q'+i)== answers[i-1]) {
+	     	score++;
+	        }	     
+	      }
+	     //Display results
+	     var results =document.getElementById('results');
+	     results.innerHTML = '<h3>You scored<span> '+score+' </span>out of <span> '+total+'</span></h3>';	     
+	     alert('You scored  '+score+' out of  '+ total);		     
+	     return false;	
 }        //End of function submitAnswers()
